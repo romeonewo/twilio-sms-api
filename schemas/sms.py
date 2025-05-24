194e9383a0c802d66723b37a79005409b22b2947
Romeo509 <romeoameku503@gmail.com>
2025-05-24 02:46:22 +0000

    revert changes

--- schemas/sms.py
+++ schemas/sms.py
@@ -3,8 +3,3 @@ from pydantic import BaseModel
 class SMSRequest(BaseModel):
     to: str
     message: str
-
-class AlertRequest(BaseModel):
-    to: str
-    alert_type: str  # e.g., "weather", "security", etc.
-    content: str     # The actual message content
